2eff8d04003c41dc67ab21625bbac7bbd9e9cd67
Soumyajeet Seal <soumyajeet009@gmail.com>
2017-05-28 00:12:44 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,33 @@
+<?php
+include_once('../../../wp-load.php');
+global $wpdb;
+if(count($_POST)>0)
+{
+	$usedata=array();	
+	
+	$user_email=isset($_REQUEST['email'])?trim($_REQUEST['email']):"";
+	$password=isset($_REQUEST['password'])?trim($_REQUEST['password']):"";
+	
+        //die();
+$user_login = sanitize_user($user_email);
+
+$creds = array();
+$creds['user_login']    = $user_login;
+$creds['user_password'] = $password;
+//$creds['remember']      = $auto_login;
+
+$user = wp_signon( $creds, false );
+
+	if ( !is_wp_error($user) ) 
+	{
+	  //header("location:".get_permalink(165));
+            wp_redirect( home_url()."/my-account" );
+	}
+	else
+	{		
+	  //header("location:".get_permalink(228));
+            wp_redirect( home_url()."/login" );
+	}
+			
+}
+?>
